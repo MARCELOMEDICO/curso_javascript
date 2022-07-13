b3c85adb73629a9fa940cf73915b49fd1c244088
marcelo medico <marcelot217@gmail.com>
2022-07-13 13:50:46 -0400

    inicianado la carga del javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let numero1=prompt("ingrese primer valor")
+let numero2=prompt("ingrese segundo valor")
+
+
+let suma= parseInt(numero1)+parseInt(numero2)
+
+alert("el resultado de la suma es: " +suma)
+
+
