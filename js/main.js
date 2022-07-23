8ba417aad0f3f1b9302083d789f3efa162fbef85
marcelo medico <marcelot217@gmail.com>
2022-07-23 02:52:01 -0400

    desafio 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-let numero1=prompt("ingrese primer valor")
-let numero2=prompt("ingrese segundo valor")
+function noesAdulto(){
+    alert("Chau Adios")
+}
+document.getElementById("boton").onclick=function(){
+    let edad= prompt("cual es tu edad")
+    let pregunta= prompt("numero 2 es par o impar??")
+    let detener=false
+
+    while (edad != ""&& pregunta != ""&&  detener==false){
+        if(edad<18){
+            let edad2=18 - edad;
+            for(edad2;edad2>=0;edad2--)
+            alert("te faltan " + edad2 + " para ser mayor")
+            alert("suerte para la proxima")
+            detener=true;
+        }else{
+            if(pregunta != "par"){
+            alert("responde mejor para la proxima")
+            detener=true;
+        }else{
+            alert("ERES APTO")
+            detener=true
+        }
+        }
 
 
-let suma= parseInt(numero1)+parseInt(numero2)
-
-alert("el resultado de la suma es: " +suma)
-
-
+        
+    }
+}
